d89d334339d372a2303bc87f10ffdf559058feb8
Arka Adhya <aadhya@onesourcevirtual.com>
2016-07-06 21:55:09 +0000

    -Add todos and todos id

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Pay Credit card bill',
+    completed: true
+}];
 
 app.get('/', function(req, res){
    res.send('ToDo API Root'); 
 });
+
+app.get('/todos', function(req, res){
+    res.json(todos);
+})
+
+app.get('/todos/:id', function(req, res){
+    //res.send("Asking for the todo with the id of " + req.params.id)
+    var matchedTodo;
+    
+    todos.forEach(function(todoItem){
+        if (todoItem.id === parseInt(req.params.id, 10)) {
+            matchedTodo = todoItem;
+        } 
+    });
+    
+    if (matchedTodo)
+    {
+        res.json(matchedTodo);
+    }
+    else{
+        res.status(404).send();
+    }
+})
 
 //Not reserved
 app.listen(PORT, process.env.IP, function(){
